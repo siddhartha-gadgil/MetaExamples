e9a7cd5e39fef9f724fd9b0a76884c2c882162c3
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-09-05 11:44:17 +0530

    lake manifest

--- MetaExamples/Basic.lean
+++ MetaExamples/Basic.lean
@@ -1,1 +1,16 @@
+import Lean
+open Lean Meta Elab
+
+elab "#stx" "[" t:term "]" : command => do
+  logInfo m!"Syntax: {t};\n{repr t}"
+
+elab "#expr" "[" t:term "]" : command =>
+  Command.liftTermElabM do
+  let t ← Term.elabTerm t none
+  let t ← instantiateMVars t
+  logInfo m!"Expression: {t}:\n{repr t}"
+  let t ← reduce t
+  let t ← instantiateMVars t
+  logInfo m!"Reduced: {t}:\n{repr t}"
+
 def hello := "world"
