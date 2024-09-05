9e0efca4922655d297c3d0d96aa620b3f3269cfd
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-09-05 11:40:50 +0530

    initial commit

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "MetaExamples" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib"
+
+@[default_target]
+lean_lib «MetaExamples» where
+  -- add any library configuration options here
