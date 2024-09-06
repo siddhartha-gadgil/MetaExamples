15aa3eb1f78865ee8b61ed6ee706c4a033d4f4b3
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-09-06 14:12:22 +0530

    update after fresh intro lecture

--- MetaExamples/Intro.lean
+++ MetaExamples/Intro.lean
@@ -1,12 +1,26 @@
 import MetaExamples.Basic
 open Lean Meta
 
+/-!
+# Meta-programming
+
+* Meta-programming is writing functions/programs that *work with* programs or their components.
+* The principled way to do this is to work with **internal representations** of program components.
+* In Lean, there are two levels of internal representation, `Syntax` and `Expr`.
+* Code is processed in two steps, **Parsing** transforms text to `Syntax` and **Elaboration** transforms `Syntax` (for terms) to `Expr`.
+-/
 #eval 1 + 3
 
 #check fun n: Nat ↦ n + 1
 
 #eval (fun n: Nat ↦ n + 1) 3
 
+/-!
+## Syntax
+
+* `Syntax` is an inductive type that gives a rooted tree structure.
+* `Syntax` is flexible, can represent all sorts of stuff, and is extensible.
+-/
 /-
 inductive Lean.Syntax : Type
 number of parameters: 0
@@ -19,11 +33,20 @@ Lean.Syntax.ident : SourceInfo → Substring → Name → List Syntax.Preresolve
 #print Syntax
 
 #stx [1]
-#stx [1 + 2]
+#stx [1 + 3]
 #stx [fun n : Nat ↦ n + 1]
 
+/-!
+## Expr
+
+* `Expr` is an inductive type representing terms in the foundations of Lean.
+-/
+
+#expr [Nat]
+#expr [Nat → Nat]
 #expr [(1: Nat) + 2]
 #expr [fun n: Nat ↦ n + 1]
+
 /-
 inductive Lean.Expr : Type
 number of parameters: 0
