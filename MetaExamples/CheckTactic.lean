7b6ffb6d606993d8024b6e64aa49971b0931f1a4
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-09-07 18:28:20 +0530

    before second video

--- MetaExamples/CheckTactic.lean
+++ MetaExamples/CheckTactic.lean
@@ -5,44 +5,49 @@ open Lean Meta Elab Tactic
 /-!
 ## Checking tactics
 
+We write a function to correctly check if a tactic closes a goal.
 -/
 
-/-
-⊢ MVarId →
-  Syntax →
-    optParam Term.Context
-        { declName? := none, auxDeclToFullName := ∅, macroStack := [], mayPostpone := true, errToSorry := true,
-          autoBoundImplicit := false,
-          autoBoundImplicits :=
-            { root := PersistentArrayNode.node (Array.mkEmpty PersistentArray.branching.toNat),
-              tail := Array.mkEmpty PersistentArray.branching.toNat, size := 0, shift := PersistentArray.initShift,
-              tailOff := 0 },
-          autoBoundImplicitForbidden := fun x ↦ false, sectionVars := ∅, sectionFVars := ∅, implicitLambda := true,
-          heedElabAsElim := true, isNoncomputableSection := false, ignoreTCFailures := false, inPattern := false,
-          tacticCache? := none, tacSnap? := none, saveRecAppSyntax := true, holesAsSyntheticOpaque := false } →
-      optParam Term.State
-          { levelNames := [], syntheticMVars := ∅, pendingMVars := ∅, mvarErrorInfos := [], mvarArgNames := ∅,
-            letRecsToLift := [] } →
-        MetaM (List MVarId × Term.State)
--/
 #check Elab.runTactic
 
-def checkTactic (target : Expr) (tac : Syntax) : TermElabM (Option Nat) :=
-  withoutModifyingState do
-    let goal ← mkFreshExprMVar target
+def checkTactic (target: Expr)(tac: Syntax):
+  TermElabM (Option Nat) :=
+    withoutModifyingState do
     try
-      let (goals, _) ← runTactic goal.mvarId! tac (← read) (← get)
+      let goal ← mkFreshExprMVar target
+      let (goals, _) ←
+        Elab.runTactic goal.mvarId! tac
+          (← read) (← get)
       return some goals.length
     catch _ =>
       return none
 
 elab "check_tactic" tac:tacticSeq : tactic => do
-    let n? ← checkTactic (← getMainTarget) tac
-    match n? with
-    | some k => logInfo m!"Tactic succeeded; goals remaining: {k}"
-    | none => logWarning "Tactic failed"
+  let n? ← checkTactic (← getMainTarget) tac
+  match n? with
+  | some n =>
+    logInfo m!"Tactic succeeded; {n} goals remain"
+  | none =>
+    logWarning m!"Tactic failed"
 
-example : 1 ≤ 3 := by
-    check_tactic rfl
-    check_tactic decide
-    decide
+example : 1 ≤ 5 := by
+  check_tactic rfl
+  check_tactic decide
+  decide
+
+syntax (name := check_tactic) "check_tactic?" tacticSeq : tactic
+@[tactic check_tactic] def checkTacticImpl : Tactic := fun stx => do
+ match stx with
+ | `(tactic| check_tactic? $tac) =>
+  let n? ← checkTactic (← getMainTarget) tac
+  match n? with
+  | some n =>
+    logInfo m!"Tactic succeeded; {n} goals remain"
+    TryThis.addSuggestion stx tac
+  | none =>
+    logWarning m!"Tactic failed"
+ | _ => throwUnsupportedSyntax
+
+example : 1 ≤ 5 := by
+  check_tactic? rfl
+  decide
