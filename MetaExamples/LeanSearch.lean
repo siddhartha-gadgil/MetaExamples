7a6548f8432c8f3df41f9109821748fce270cca6
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-09-10 18:46:23 +0530

    fixed terms, tactics ungrouped

--- MetaExamples/LeanSearch.lean
+++ MetaExamples/LeanSearch.lean
@@ -1,6 +1,6 @@
 import Lean
 import Mathlib
-open Lean Meta Elab Tactic Parser
+open Lean Meta Elab Tactic Parser Term
 
 def getQueryJson (s: String)(num_results : Nat := 12) : IO <| Array Json := do
   let apiUrl := "https://leansearch.net/api/search"
@@ -37,15 +37,38 @@ def query (s: String)(num_results : Nat := 12) :
 
 def toCommandSuggestion (sr : SearchResult) : TryThis.Suggestion :=
   let data := match sr.docString? with
-    | some doc => s!"· Description: {doc}\n"
+    | some doc => s!"{doc}\n"
     | none => ""
-  let data := data ++ match sr.type? with
-    | some type => s!"· Type: {type}\n"
-    | none => ""
-  let data := data ++ match sr.doc_url? with
-    | some docurl => s!"· URL: {docurl}\n"
-    | none => ""
+  -- let data := data ++ match sr.type? with
+  --   | some type => s!"· Type: {type}\n"
+  --   | none => ""
+  -- let data := data ++ match sr.doc_url? with
+  --   | some docurl => s!"· URL: {docurl}\n"
+  --   | none => ""
   {suggestion := s!"#check {sr.name}", postInfo? := sr.type?.map fun s => s!" -- {s}" ++ s!"\n{data}"}
+
+def toTermStx? (sr: SearchResult) : MetaM <| Option Syntax :=
+  sr.type?.bindM fun type => do
+  let term? := runParserCategory (← getEnv) `term type
+  return term?.toOption
+
+def toTermSuggestion (sr: SearchResult) : TryThis.Suggestion :=
+  match sr.type? with
+  | some type => {suggestion := sr.name, postInfo? := some s!" (type: {type})"}
+  | none => {suggestion := sr.name}
+
+def toTermSuggestions (sr: SearchResult) : Array TryThis.Suggestion :=
+  match sr.type? with
+  | some type => #[{suggestion := sr.name, postInfo? := some s!" (type: {type})"}, {suggestion := type, preInfo? := some "Type: "}, {suggestion := s!"({sr.name} : {type})"}]
+  | none => #[{suggestion := sr.name}]
+
+def toTacticSuggestions (sr: SearchResult) : Array TryThis.Suggestion :=
+  match sr.type? with
+  | some type => #[{suggestion := s!"apply {sr.name}", postInfo? := some s!" (type: {type})"},
+        {suggestion := s!"have : {type} := {sr.name}", postInfo? := some s!" (type: {type})"},
+        {suggestion := s!"rw [sr.name]", postInfo? := some s!" (type: {type})"},
+        {suggestion := s!"rw [← sr.name]", postInfo? := some s!" (type: {type})"}]
+  | none => #[]
 
 end SearchResult
 
@@ -53,6 +76,30 @@ def getQueryCommandSuggestions (s: String)(num_results : Nat := 12) :
   IO <| Array TryThis.Suggestion := do
     let searchResults ←  SearchResult.query s num_results
     return searchResults.map SearchResult.toCommandSuggestion
+
+def getQueryTermSuggestions (s: String)(num_results : Nat := 12) :
+  IO <| Array TryThis.Suggestion := do
+    let searchResults ←  SearchResult.query s num_results
+    return searchResults.map SearchResult.toTermSuggestion
+
+def getQueryTacticSuggestions (s: String)(num_results : Nat := 12) :
+  IO <| Array TryThis.Suggestion := do
+    let searchResults ←  SearchResult.query s num_results
+    return searchResults.map SearchResult.toTacticSuggestions |>.join
+
+def getQueryTacticSuggestionGroupss (s: String)(num_results : Nat := 12) :
+  IO <| Array <| Array TryThis.Suggestion := do
+    let searchResults ←  SearchResult.query s num_results
+    return searchResults.map SearchResult.toTacticSuggestions
+
+def defaultTerm (expectedType? : Option Expr) : MetaM Expr := do
+  match expectedType? with
+  | some type =>
+    if !type.hasExprMVar then
+      mkAppM ``sorryAx #[type, mkConst ``false]
+    else
+      return mkConst ``True.intro
+  | none => return mkConst ``True.intro
 
 open Command
 syntax (name := lean_search_cmd) "#lean_search" str : command
@@ -68,15 +115,48 @@ syntax (name := lean_search_cmd) "#lean_search" str : command
       logWarning "Lean search query should end with a full stop (period) or a question mark."
   | _ => throwUnsupportedSyntax
 
+syntax (name := lean_search_term) "lean_search#" str : term
+@[term_elab lean_search_term] def leanSearchTermImpl : TermElab :=
+  fun stx expectedType? => do
+  match stx with
+  | `(lean_search# $s) =>
+    let s := s.getString
+    if s.endsWith "." || s.endsWith "?" then
+      let suggestions ← getQueryTermSuggestions s
+      TryThis.addSuggestions stx suggestions (header:= "Lean Search Results")
+    else
+      logWarning "Lean search query should end with a full stop (period) or a question mark."
+    defaultTerm expectedType?
+  | _ => throwUnsupportedSyntax
+
+syntax (name := lean_search_tactic) "lean_search?" str : tactic
+@[tactic lean_search_tactic] def leanSearchTacticImpl : Tactic :=
+  fun stx => do
+  match stx with
+  | `(tactic|lean_search? $s) =>
+    let s := s.getString
+    if s.endsWith "." || s.endsWith "?" then
+      let suggestions ← getQueryTacticSuggestions s
+      TryThis.addSuggestions stx suggestions (header:= "Lean Search Results")
+    else
+      logWarning "Lean search query should end with a full stop (period) or a question mark."
+  | _ => throwUnsupportedSyntax
+
+-- example := lean_search# "There are infinitely many odd numbers."
 /-!
-# Lean Search Command
+# Lean Search Examples
 
 An example of using the leansearch API. The search is triggered when the sentence ends with a full stop (period) or a question mark.
 -/
 
 #lean_search "There are infinitely many odd numbers"
 
-#lean_search "delay task (use `Task.await`)"
+example := lean_search# "There are infinitely many odd numbers."
+
+example : True := by
+  skip
+  lean_search? "There are infinitely many odd numbers."
+  sorry
 
 open Parser
 #check Parser
