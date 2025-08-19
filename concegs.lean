9da9fcec5f48ee83413f0dc26ab29135d5444566
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-08-19 12:03:17 +0000

    loop with spawning, repeating inputs

--- concegs.lean
+++ concegs.lean
@@ -4,14 +4,19 @@ open Std.Internal.IO Async Std
 
 def writeSlow (n: Nat) (s: String := "")(count : Std.Mutex Nat) : Async Nat := do
   IO.sleep (n * 1000).toUInt32
-  IO.FS.writeFile "slow.txt" s!"This is a slow write operation after {n} seconds."
-  IO.eprintln s!"Write operation completed after {n} seconds."
-  unless s.isEmpty do
-    IO.eprintln s!"Additional input: {s}"
+  IO.eprintln s!"Write operation completed after {n} seconds; phase: {s}"
   count.atomically (·.modify (fun n ↦ n + 1))
   let c ← count.atomically (·.get)
   IO.eprintln s!"Total writes completed: {c}"
   return n
+
+partial def loop (n: Nat) (s: String) (count : Std.Mutex Nat) (stdin : IO.FS.Stream) : Async Unit := do
+  for m in [1:n] do
+    background (prio := Task.Priority.default) (writeSlow m s count)
+  IO.eprintln s!"Write operations started in phase {s}. Waiting for input. Type 'q' to quit"
+  let inp ← stdin.getLine
+  unless inp.startsWith "q" do
+    loop n inp count stdin
 
 def main (args: List String) : IO Unit := do
   AsyncTask.block <| ←
@@ -19,9 +24,4 @@ def main (args: List String) : IO Unit := do
       let n := args[0]! |>.toNat!
       let count ←  Std.Mutex.new 0
       let stdin ← IO.getStdin
-      for m in [1:n] do
-        background (prio := Task.Priority.default) (writeSlow m "" count)
-      IO.eprintln "Write operation started, check slow.txt. Waiting for input."
-      let inp ← stdin.getLine
-      for m in [1:n] do
-        background (prio := Task.Priority.default) (writeSlow m inp count)
+      loop n "intial" count stdin
